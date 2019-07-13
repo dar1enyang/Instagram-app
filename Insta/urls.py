ce7c2e48087073b0c5527b0a07410a7c4ac465a6
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-07-13 19:58:21 -0400

    CRUD - READ: Master/Detail interface - Completed

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from Insta.views import HelloDjango
+from Insta.views import PostView, PostDetail
 
 urlpatterns = [
-    path('', HelloDjango.as_view(), name='home'),
+    path("", PostView.as_view(), name="home"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail")
+
 ]
